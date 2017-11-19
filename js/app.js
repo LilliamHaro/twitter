4181f3afe47b7802103bae848b9672452a2ca482
LilliamHaro <qualia950@gmail.com>
2017-11-19 13:11:12 -0500

    subiendo codigo comentado

--- js/app.js
+++ js/app.js
@@ -1,50 +1,92 @@
-window.onload = function() {
-var buttonTweet = document.getElementById('buttonT');//definiendo el elemento que desencadenara el evento
-buttonTweet.addEventListener('click',tweeting); //agregando evento y llamando a la fucnión que se efectuará
-}
-//Creando la funcion
-function  tweeting(event) {
- var contenedor = document.querySelector('.published'); //ubicando el luagr donde aparecerá el nuevo tweet
- var texto = document.form.text.value; // obteniendo el texto que escribiremos en el tweet
- // validando si el campo de texto esta vacio
-   if(texto == 0){
-     document.getElementById('buttonT').disabled = true; //desabilitando el boton
-   }
-   else {
-		 var newTweetBox = document.createElement('p'); //creando elemento contenedor
-     // newTweetBox.innerHTML = firstLine + '<br>' + seconLine + '<br>' + thirdLine; // dandole el texto que escribimos al elemento contenedor
-     newTweetBox.innerHTML = texto;
-		 document.getElementById('newTweets').appendChild(newTweetBox); //agregando el contenedor con el texto al a la caja de nuevos tweets
-     document.form.text.value = ""; //vaciando el contenedor de texto una vez se haya publicado
-   }
-}
-var tweet = document.form.text; //definiendo el elemento que escuchara el evento key down
+window.addEventListener('load', function() {
+  // definiendo el elemento que escuchara en el evento click
+  var buttonTweet = document.getElementById('buttonT');
+  // definiendo el elemento que escuchara en el evento keydown
+  var textArea = document.getElementById('text-area');
+  // -------------------------------------------------EVENTO KEYDOWN---------------------------------------
+  // podríamos haber usado el evento keyup pero el contador no se hubiera mostrado como deseamos
+  textArea.addEventListener('keydown', function() {
+    // definiendo el numero de caracteres que se han tecleado en el text-area
+    var numsOfCharacters = textArea.value.length;
+    // limite de caracteres posibles en nuestro tweet contando desde el primer caracter
+    var limit = 139;
+    // dandole valor al contador
+    // contador = limit - numsOfCharacters -- le hará saber al usauro cuantos carateres le quedan
+    document.getElementById('countingNums').innerHTML = limit - numsOfCharacters;
+    // codicionando contador
+    if (numsOfCharacters >= 0 && numsOfCharacters <= 120) {
+      buttonTweet.disabled = false;
+      buttonTweet.setAttribute('class', 'buttonTweet sky-bg');
+      document.getElementById('countingNums').setAttribute('class', 'characters-nums');
+    } else if (numsOfCharacters > 120 && numsOfCharacters <= 130) {
+      buttonTweet.disabled = false;
+      buttonTweet.setAttribute('class', 'buttonTweet sky-bg');
+      document.getElementById('countingNums').setAttribute('class', 'characters-nums green-nums');
+    } else if (numsOfCharacters > 130 && numsOfCharacters < 140) {
+      buttonTweet.disabled = false;
+      buttonTweet.setAttribute('class', 'buttonTweet sky-bg');
+      document.getElementById('countingNums').setAttribute('class', 'characters-nums red-nums');
+    } else if (numsOfCharacters >= 140) {
+      buttonTweet.disabled = true;
+      buttonTweet.setAttribute('class', 'buttonTweet');
+      document.getElementById('countingNums').setAttribute('class', 'characters-nums gray-nums');
+    }
+    // Condicionando el evento key para cambiar el tamaño del textarea al dar enter
+    // 13 ---> código ascii de la tecla enter
+    if (event.keyCode === 13) {
+      // para darle el tamaño del scroll a la altura del textarea
+      textArea.style.height = (textArea.scrollHeight) + 'px';
+    }
+  });
+  // ----------------------------------------------------EVENTO CLICK-----------------------------------------------
 
-//agregando la el evento y la funcion para el conteo de caracteres
-tweet.addEventListener("keydown",function(){
-  var numsOfCharacters = tweet.value.length;
-  var limit = 140;
+  // volveremos a usar las variables   buttonTweet y textArea definidas en la parte superior
+  buttonTweet.addEventListener('click', function(event) {
+    // definimos el contenedor donde aparecerán los nuesvos tweets
+    var container = document.getElementById('newTweets');
+    // definiendo variables para obtener la hora -- usando el objeto date
+    var fecha = new Date();
+    var hora = fecha.getHours();
+    var minuto = fecha.getMinutes();
+    // condicionales para evitar que las horas y los minutos no aparezcan como unidades ej: '9'--> '09'
+    if (minuto < 10) {
+      minuto = '0' + minuto;
+    }
+    if (hora < 10) {
+      hora = '0' + hora;
+    }
+    var hours = 'Publicado a las ' + hora + ':' + minuto + ' horas';
 
- document.getElementById("countingNums").innerHTML = limit - numsOfCharacters;
- if(numsOfCharacters > 140){
-	 document.getElementById('buttonT').disabled = true;
-	 document.getElementById('buttonT').style.backgroundColor = 'gray';
-	 document.getElementById("countingNums").style.color = 'gray';
-
-
- }
- else if(numsOfCharacters > 130 && numsOfCharacters < 140) {
-	 document.getElementById("countingNums").style.color = 'red';
-	 document.getElementById('buttonT').disabled = false;
-	 document.getElementById('buttonT').style.backgroundColor = '#6ad2f3';
- }
- else if(numsOfCharacters > 120) {
-	document.getElementById("countingNums").style.color = 'green';
-	document.getElementById('buttonT').disabled = false;
-	document.getElementById('buttonT').style.backgroundColor = '#6ad2f3';
- }
- else if(numsOfCharacters < 120){
-	 document.getElementById("countingNums").style.color = '#6ad2f3';
-	 document.getElementById('buttonT').disabled = false;
- }
+    // validamos que el textarea no este vacío ni este hecho de solo espacios
+    if (textArea.value != 0) {
+      // creando nuevo tweet
+      // creando contenedor para la hora
+      var newTweetsHour = document.createElement('p');
+      // dandole el texto escrito en el textarea al contenedor del tweet
+      newTweetsHour.innerHTML = hours;
+      // creando contenedor para el uevo tweet
+      var newTweetBox = document.createElement('div');
+      // dandole el valor del texto escrito en el textarea al contenedor del tweet
+      newTweetBox.innerHTML = textArea.value;
+      // uniendo el contenedor con la hora al tweet
+      newTweetBox.appendChild(newTweetsHour);
+      // Condicionando la posicion del nuevo tweet
+      // si no hay tweets anteriores se agregara normalmente
+      if (!container.children) {
+        container.appendChild(newTweetBox);
+      } else {
+        // si hay tweets anteriores el nuevo tweet se agregará antes que estos
+        container.insertBefore(newTweetBox, container.firstElementChild);
+      }
+      // reiniando el textarea
+      textArea.value = '';
+      // reiniciando el contador
+      document.getElementById('countingNums').innerHTML = 140;
+      // regresamos al boton y al contador a su clase original
+      buttonTweet.setAttribute('class', 'buttonTweet');
+      document.getElementById('countingNums').setAttribute('class', 'characters-nums');
+      // reiniciando el tamaño del textarea en el caso de que el contenido haya sobrepasado su altura
+      document.getElementById('text-area').style = 'initial';
+    }
+  });
 });
